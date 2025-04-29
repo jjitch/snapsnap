2f8a8964307ec3dc620c28c9f22a834e1ba10837
jjitch <junnosuke_08@icloud.com>
2025-04-29 20:30:21 +0900

    move App source to view directory to clearify its responsibility

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import { Image } from "@tauri-apps/api/image";
 import "./App.css";
 
 function App() {
   return (
     <main className="container">
-      <p>Tauri</p>
     </main>
   );
 }
