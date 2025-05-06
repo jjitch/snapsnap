6fb0a8be0437597827da9279694174d698de35d7
jjitch <junnosuke_08@icloud.com>
2025-05-06 21:45:43 +0900

    add dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ async fn one_time_shoot() -> tauri::ipc::Response {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![one_time_shoot])
         .run(tauri::generate_context!())
