ed801a50e611f80cd730ad97498d0333addf55fa
jjitch <junnosuke_08@icloud.com>
2025-05-16 08:22:45 +0900

    handling padding
    frame buffer somtimes contains padding
    See also https://learn.microsoft.com/en-us/windows/win32/medfound/image-stride

--- src-tauri/src/snap.rs
+++ src-tauri/src/snap.rs
@@ -5,7 +5,7 @@ pub fn snap() -> ImageBin {
     ImageBin::new(img_loadeded)
 }
 
-use std::sync::mpsc;
+use std::{io::Write, sync::mpsc};
 
 use windows_capture::{
     capture::{Context, GraphicsCaptureApiHandler},
@@ -16,25 +16,40 @@ use windows_capture::{
     settings::{ColorFormat, CursorCaptureSettings, DrawBorderSettings, Settings},
 };
 
+#[derive(Default)]
+pub struct OwnedFrame {
+    captured_data: Vec<u8>,
+    width: u32,
+    height: u32,
+}
+
+impl OwnedFrame {
+    pub fn save_as_jpeg(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let file = std::fs::File::create(path)?;
+        let encoder = ImageEncoder::new(ImageFormat::Jpeg, ColorFormat::Bgra8);
+        let jpg_data = encoder.encode(&self.captured_data, self.width, self.height)?;
+        let mut writer = std::io::BufWriter::new(file);
+        writer.write(&jpg_data)?;
+        writer.flush()?;
+        Ok(())
+    }
+}
+
 struct Capture {
-    encoder: Option<ImageEncoder>,
-    output_channel: mpsc::Sender<Vec<u8>>,
+    owned_frame: mpsc::Sender<OwnedFrame>,
 }
 
 impl GraphicsCaptureApiHandler for Capture {
     // The type of flags used to get the values from the settings.
-    type Flags = mpsc::Sender<Vec<u8>>;
+    type Flags = mpsc::Sender<OwnedFrame>;
 
     // The type of error that can be returned from `CaptureControl` and `start` functions.
     type Error = Box<dyn std::error::Error + Send + Sync>;
 
     // Function that will be called to create a new instance. The flags can be passed from settings.
     fn new(ctx: Context<Self::Flags>) -> Result<Self, Self::Error> {
-        let encoder = ImageEncoder::new(ImageFormat::Bmp, ColorFormat::Rgba8);
-
         Ok(Self {
-            encoder: Some(encoder),
-            output_channel: ctx.flags,
+            owned_frame: ctx.flags,
         })
     }
 
@@ -43,25 +58,19 @@ impl GraphicsCaptureApiHandler for Capture {
         frame: &mut Frame,
         capture_control: InternalCaptureControl,
     ) -> Result<(), Self::Error> {
-        let width = frame.width();
-        let height = frame.height();
-        let encoded_data = self.encoder.as_mut().unwrap().encode(
-            frame.buffer().unwrap().as_raw_buffer(),
-            width,
-            height,
-        )?;
+        self.owned_frame.send(OwnedFrame {
+            captured_data: frame.buffer()?.as_nopadding_buffer()?.to_vec(),
+            width: frame.width(),
+            height: frame.height(),
+        })?;
         capture_control.stop();
-        self.output_channel
-            .send(encoded_data)
-            .map_err(|_| "Failed to send data to channel")?;
         Ok(())
     }
 }
 
-pub fn take_screenshot() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn take_screenshot() -> Result<OwnedFrame, Box<dyn std::error::Error>> {
     // Gets the foreground window, refer to the docs for other capture items
     let primary_monitor = Monitor::primary().expect("There is no primary monitor");
-
     let (tx, rx) = mpsc::channel();
 
     let settings = Settings::new(
@@ -71,10 +80,7 @@ pub fn take_screenshot() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         ColorFormat::Rgba8,
         tx,
     );
-
-    // Starts the capture and takes control of the current thread.
-    // The errors from handler trait will end up here
-    Capture::start(settings)?;
-
-    Ok(rx.recv()?)
+    Capture::start_free_threaded(settings)?;
+    let owned_frame = rx.recv().expect("Failed to receive frame");
+    Ok(owned_frame)
 }
