4f6b7d433cd17340d47362b4568e2df63e08395d
jjitch <junnosuke_08@icloud.com>
2025-05-16 08:23:19 +0900

    update sample code to new interface

--- src-tauri/examples/take_screenshot.rs
+++ src-tauri/examples/take_screenshot.rs
@@ -1,13 +1,8 @@
-use std::io::Write;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let now = std::time::Instant::now();
-    let bmp = snapsnap_lib::snap::take_screenshot()?;
+    let frame = snapsnap_lib::snap::take_screenshot()?;
     let elapsed = now.elapsed();
     println!("Screenshot taken in: {:?}", elapsed);
-    let output_file = std::fs::File::create("screenshot.bmp")?;
-    let mut writer = std::io::BufWriter::new(output_file);
-    writer.write(&bmp)?;
-    writer.flush()?;
+    frame.save_as_jpeg("screenshot.jpg")?;
     Ok(())
 }
